d9265d53006389727415e7b052b9ce10b59241ce
jankat <cankatguven@gmail.com>
2023-04-13 21:37:39 +0300

    footer navs add

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,14 +1,15 @@
-import { Container } from "react-bootstrap";
+import { Container, Image } from "react-bootstrap";
 
 const About = () => {
   return (
     <div>
       <Container className="blockquote my-3">
+        <p className="display-6">Welcome to your humble library</p>
         <p>
-          Welcome to <strong>THE BOOKEST!</strong>, the ultimate destination for
-          book lovers everywhere! As an avid reader and book enthusiast, I
-          created this website to share my passion for literature and make it
-          easy for you to find the books you're looking for.
+          THE BOOKEST is the ultimate destination for book lovers everywhere! As
+          an avid reader and book enthusiast, I created THE BOOKEST to share my
+          passion for literature and make it easy for you to find the books
+          you're looking for.
         </p>
         <p>
           Whether you're searching for the latest bestsellers, timeless
@@ -18,27 +19,28 @@ const About = () => {
           in all genres, with a bestsellers section that's constantly updated
           with the latest and most popular books.
         </p>
+        <p className="display-6">Don't Get Confused with Your Books:</p>
         <p>
           One of the most exciting features of my website is the ability to
           create your own library of books. You can track your reading journey
           and keep a record of the books you've read, plan to read, and those
           you've taken notes on.
         </p>
+        <p className="display-6">No Need for a Pen or Paper:</p>
         <p>
           I know that many readers like to take notes while they're reading,
           whether it's to jot down their thoughts or to remember important
           details. That's why I've created a tool that lets you take notes right
-          on my website, without the need for a pen or paper. with
+          on my website, without the need for a pen or paper. With the
           note-taking tool, you can write notes for any book you're reading and
           save them to your "books with notes" shelf. This makes it easy to go
           back and reference your thoughts and observations about a book.
         </p>
         <p>
-          At <strong>THE BOOKEST</strong>, my goal is to make the reading
-          experience as enjoyable and seamless as possible. Whether you're a
-          lifelong book lover or just getting started, I hope you'll find my
-          website to be a valuable resource and a fun place to explore. Happy
-          reading!
+          At THE BOOKEST, my goal is to make the reading experience as enjoyable
+          and seamless as possible. Whether you're a lifelong book lover or just
+          getting started, I hope you'll find my website to be a valuable
+          resource and a fun place to explore. Happy reading!
         </p>
       </Container>
     </div>
