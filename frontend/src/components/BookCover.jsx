622b61b46ad6278f03ee751846a3554a40d60752
jankat <cankatguven@gmail.com>
2023-10-29 02:23:11 +0300

    search padding add

--- frontend/src/components/BookCover.jsx
+++ frontend/src/components/BookCover.jsx
@@ -50,7 +50,7 @@ const BookCover = ({
           <Card.Body className="text-dark border-0 w-100 d-flex flex-column justify-content-start">
             <Card.Title className="py-0 blockquote my-0 text-center w-100 border-0 ">
               {title?.length > 25
-                ? title.slice(0, 25) + "..."
+                ? title.slice(0, 25) + "..." 
                 : title || "no title"}
             </Card.Title>
             <Card.Text className="py-0 my-0 text-center">
