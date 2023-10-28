ba8697249ce3df4e7f6d6174ccb09316554ec7a5
jankat <cankatguven@gmail.com>
2023-10-28 20:26:46 +0300

    book cover ch

--- frontend/src/components/BookCover.jsx
+++ frontend/src/components/BookCover.jsx
@@ -34,7 +34,7 @@ const BookCover = ({
         className="text-decoration-none w-100"
       >
         <Card className="h-100 w-100 rounded-0 border border-gray shadow-sm book-cover-card">
-          <span className="d-flex justify-content-center pt-4" style={{minHeight: "144px",  maxHeight: "370px" }}>
+          <span className="d-flex justify-content-center pt-4" style={{minHeight: "170px",  maxHeight: "370px" }}>
             <img
               className="rounded-0 book-cover-image mx-auto" loading="lazy"
               src={image || defaultImage}
