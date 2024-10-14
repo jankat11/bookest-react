61b9cf07f2ba46f247fbaa06b405a87a57649a6f
cankat güven <cankatguven@gmail.com>
2024-10-14 21:39:46 +0300

    google api add

--- frontend/src/features/bookGenreSlice/bookGenreThunk.jsx
+++ frontend/src/features/bookGenreSlice/bookGenreThunk.jsx
@@ -1,40 +1,40 @@
 import axios from "axios";
 const BASE_URL = import.meta.env.VITE_BOOKEST_GENRE_URL;
-const GOOGLE_URL = import.meta.env.VITE_GOOGLE_URL
-const GOOGLE_API = import.meta.env.VITE_GOOGLE_API
+const GOOGLE_URL = import.meta.env.VITE_GOOGLE_URL;
+const GOOGLE_API = import.meta.env.VITE_GOOGLE_API;
 const BOOK_AMOUNT_PER_PAGE = 36;
-
-
 
 const fetchBooks = async (genre, thunkAPI) => {
   try {
-    const { data } = await axios.get(
-      `${BASE_URL}/${genre}`
-    );
-    return data.results.books
+    const { data } = await axios.get(`${BASE_URL}/${genre}`);
+    return data.results.books;
   } catch (error) {
     return thunkAPI.rejectWithValue(error.message);
   }
 };
 export default fetchBooks;
 
-
 export const searchBooks = async (searchData, thunkAPI) => {
+  const page = searchData.searchPage;
+  const start = (page - 1) * BOOK_AMOUNT_PER_PAGE;
+  const urlWithoutKey = `${GOOGLE_URL}${searchData.words}&startIndex=${start}&maxResults=${BOOK_AMOUNT_PER_PAGE}`;
+
   try {
-    const page = searchData.searchPage;
-    const start = (page - 1) * BOOK_AMOUNT_PER_PAGE
-   
-    const { data } = await axios.get(
-      `${GOOGLE_URL}${searchData.words}&startIndex=${start}&maxResults=${BOOK_AMOUNT_PER_PAGE}&key=${GOOGLE_API}`
-    );
+    const urlWithKey = `${urlWithoutKey}&key=${GOOGLE_API}`;
+    const { data } = await axios.get(urlWithKey);
     return data.items;
   } catch (error) {
+    if (error) {
+      try {
+        const { data } = await axios.get(urlWithoutKey);
+        return data.items;
+      } catch (errorWithoutKey) {
+        return thunkAPI.rejectWithValue(errorWithoutKey.message);
+      }
+    }
     return thunkAPI.rejectWithValue(error.message);
   }
-}; 
-
-
-
+};
 
 /* const fetchBooks = async (genre, thunkAPI) => {
   try {
--- frontend/src/features/bookSlice/bookDetailsThunk.jsx
+++ frontend/src/features/bookSlice/bookDetailsThunk.jsx
@@ -1,21 +1,45 @@
 import axios from "axios";
 const BASE_URL = "https://www.googleapis.com/books/v1/volumes";
 const NOTE_URL = "https://bookest-server.up.railway.app/api/users/";
-const GOOGLE_API = import.meta.env.VITE_GOOGLE_API
+const GOOGLE_API = import.meta.env.VITE_GOOGLE_API;
 /* const NOTE_URL = "http://127.0.0.1:8000/api/users/"; */
 
 export const fetchBook = async (idData, thunkAPI) => {
   try {
     const bookData = await axios.get(
-      `${BASE_URL}${idData.id ? "/" + idData.id + "?key=" + GOOGLE_API: "?q=isbn:" + idData.isbn + "&key=" + GOOGLE_API}`
+      `${BASE_URL}${
+        idData.id
+          ? "/" + idData.id + "?key=" + GOOGLE_API
+          : "?q=isbn:" + idData.isbn + "&key=" + GOOGLE_API
+      }`
     );
     let book = idData.isbn ? bookData.data.items[0] : bookData.data;
     if (idData.isbn) {
-      const { data } = await axios.get(`${BASE_URL}${"/" + book.id}?key=${GOOGLE_API}`);
+      const { data } = await axios.get(
+        `${BASE_URL}${"/" + book.id}?key=${GOOGLE_API}`
+      );
       book = data;
     }
     return book;
   } catch (error) {
+    if (error) {
+      try {
+        const bookData = await axios.get(
+          `${BASE_URL}${idData.id ? "/" + idData.id : "?q=isbn:" + idData.isbn}`
+        );
+        let book = idData.isbn ? bookData.data.items[0] : bookData.data;
+        if (idData.isbn) {
+          const { data } = await axios.get(`${BASE_URL}${"/" + book.id}`);
+          book = data;
+        }
+        return book;
+      } catch (errorWithoutKey) {
+        return thunkAPI.rejectWithValue(
+          "Sorry! the book data is not available"
+        );
+      }
+    }
+
     return thunkAPI.rejectWithValue("Sorry! the book data is not available");
   }
 };
