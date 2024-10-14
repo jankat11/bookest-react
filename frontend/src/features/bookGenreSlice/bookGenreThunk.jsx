63753ba87ab2a90eca21524936b1bd7ff0c16a29
cankat güven <cankatguven@gmail.com>
2024-10-14 14:13:08 +0300

    googleauth add

--- frontend/src/features/bookGenreSlice/bookGenreThunk.jsx
+++ frontend/src/features/bookGenreSlice/bookGenreThunk.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 const BASE_URL = import.meta.env.VITE_BOOKEST_GENRE_URL;
 const GOOGLE_URL = import.meta.env.VITE_GOOGLE_URL
+const GOOGLE_API = import.meta.env.VITE_GOOGLE_API
 const BOOK_AMOUNT_PER_PAGE = 36;
 
 
@@ -24,7 +25,7 @@ export const searchBooks = async (searchData, thunkAPI) => {
     const start = (page - 1) * BOOK_AMOUNT_PER_PAGE
    
     const { data } = await axios.get(
-      `${GOOGLE_URL}${searchData.words}&startIndex=${start}&maxResults=${BOOK_AMOUNT_PER_PAGE}` + "&key=AIzaSyA20VANwd23uZhM65g0pjRupT0Jwc5rXs8"
+      `${GOOGLE_URL}${searchData.words}&startIndex=${start}&maxResults=${BOOK_AMOUNT_PER_PAGE}&key=${GOOGLE_API}`
     );
     return data.items;
   } catch (error) {
